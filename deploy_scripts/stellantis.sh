ba7c1844e95b4411de1a136a5eac7d9358331de7
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2024-07-09 11:33:21 +0100

    add my Nextjs deploy script

---
+++ deploy_scripts/stellantis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Deploying Stellantis VENG Project Manager"
+
+cd /var/www/stellantis-veng-project-manager/
+git pull origin main
+systemctl restart postgresql
+npm install
+npx prisma db push --accept-data-loss
+npx prisma generate
+npx @digitak/esrun seed/seed.ts
+npx next build
+npx next start --port 7887
+systemctl restart stellantis
